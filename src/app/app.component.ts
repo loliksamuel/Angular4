2345d16fdce252a10a9fe463241e7f8a81123a36
Samuel Cardonis <samuelcardonis@Samuels-MacBook-Pro.local>
2017-07-06 15:58:54 +0300

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  name = 'sam';
- lastName = 'app';
- city = 'app';
- wife = 'app';
+    counter   :number       = 1;
+    title     :string       = 'app';
+    firstName :string       = 'sam';
+    lastName  :string       = 'Car';
+    city      :string       = 'TLV';
+    skills    =["java", "javascript", "angular", "spring", "spring boot", "jhipster"];
+    isMarried :boolean      = false;
+    age       :number       =   20;
+
+    //event binding
+    onButtonClick(){
+      this.title  = this.title  + this.counter++;
+    }
+
+    //event binding
+    onCheckboxClick(){
+      this.isMarried = !this.isMarried;
+    }
 }
